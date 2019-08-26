81a65640dad241892c227d4e1b684b827831716c
Justin <justin.vietz@gmx.de>
2019-08-26 21:04:49 +0200

    Added star file to export

--- lib/ripple_color_selection.dart
+++ lib/ripple_color_selection.dart
@@ -4,3 +4,4 @@ export 'controller/color_selection_controller.dart';
 // tiles
 export 'widgets/tiles/circle_tile.dart';
 export 'widgets/tiles/rectangle_tile.dart';
+export 'widgets/tiles/star_tile.dart';
